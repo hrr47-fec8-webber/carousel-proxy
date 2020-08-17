0d3ca439332b8dd5d3b23226892927c325c774dc
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-08-17 12:09:54 -0700

    Basic proxy server with Gallery component

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const axios = require('axios');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.use(express.static(path.join(__dirname, '/../client')));
+app.use('/:id', express.static(path.join(__dirname, '/../client')));
+
+app.use(bodyParser.json());
+
+app.get('/api/images/:id', (req, res) => {
+  const id = req.url.slice(12);
+  axios.get(`http://localhost:3001/api/images/${id}`)
+    .then((data) => res.send(data.data))
+    .catch((error) => console.log(error));
 });
 
 app.listen(port, () => {
