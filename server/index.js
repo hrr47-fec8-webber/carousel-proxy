bd2d12200625255415931588e2e64e4de6981307
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-08-17 17:30:04 -0700

    Begin incorporating others' modules

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ const path = require('path');
 const app = express();
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, '/../client')));
 app.use('/:id', express.static(path.join(__dirname, '/../client')));
 
@@ -13,6 +15,13 @@ app.use(bodyParser.json());
 app.get('/api/images/:id', (req, res) => {
   const id = req.url.slice(12);
   axios.get(`http://localhost:3001/api/images/${id}`)
+    .then((data) => res.send(data.data))
+    .catch((error) => console.log(error));
+});
+
+app.get('/api/booking/:id', (req, res) => {
+  const id = req.url.slice(13);
+  axios.get(`http://localhost:3002/api/booking/${id}`)
     .then((data) => res.send(data.data))
     .catch((error) => console.log(error));
 });
